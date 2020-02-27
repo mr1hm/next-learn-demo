c90676d9121379289626e2ffbded696d8b86e22f
Kevin <kevinihm@gmail.com>
2020-02-27 15:21:14 -0800

    api-routes

---
+++ 7.2-api-routes/pages/api/randomQuote.jsx
@@ -0,0 +1,13 @@
+import allQuotes from '../../quotes.json';
+
+export default (req, res) => {
+  const { author } = req.query;
+  let quotes = allQuotes;
+
+  if (author) quotes = quotes.filter(q => q.author.toLowerCase().includes(author.toLowerCase()));
+  if (!quotes.length) quotes = allQuotes.filter(q => q.author.toLowerCase() === 'unknown');
+
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  res.status(200).json(quote);
+};
